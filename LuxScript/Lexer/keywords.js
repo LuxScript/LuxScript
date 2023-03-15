21e50b4b61eaa316023213a13e21023d69afe7e2
LuxScript <123044206+LuxScript@users.noreply.github.com>
2023-03-15 18:38:07 -0400

    Delete LuxScript directory

--- LuxScript/Lexer/keywords.js
+++
@@ -1,8 +0,0 @@
-const KEYWORDS = {
-    if: "IF",
-    else: "ELSE",
-    while: "WHILE",
-    for: "FOR",
-    function: "FUNCTION",
-};
-module.exports = KEYWORDS;
--- LuxScript/Lexer/tokentype.js
+++
@@ -1,9 +0,0 @@
-const TokenType = {
-    IDENTIFIER: "IDENTIFIER",
-    NUMBER: "NUMBER",
-    STRING: "STRING",
-    OPERATOR: "OPERATOR",
-    KEYWORD: "KEYWORD",
-    PUNCTUATION: "PUNCTUATION",
-};
-module.exports = TokenType;
